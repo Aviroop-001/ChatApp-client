375d1e4e691d56df9e4b5c94f25723255a1812ad
Aviroop-001 <banerjeeaviroop01@gmail.com>
2022-12-19 23:28:07 +0530

    migrated server from Heroku to render

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `https://aviroop-chatapp-api.herokuapp.com/api/`
+  // baseURL: `https://aviroop-chatapp-api.herokuapp.com/api/`
+  baseURL: `https://radius-server.onrender.com/api`
 });
